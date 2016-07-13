2433f8a599af9e27e507ef3c29e0e5af1c2ea46a
Matt Plavcan <mwplavcan@gmail.com>
2016-07-13 16:20:51 -0700

    Added Visual Studio project structure

---
+++ src/DeviceTest/DeviceTest.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		DeviceTest.ino
+ Created:	7/13/2016 4:19:16 PM
+ Author:	mplavcan
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
