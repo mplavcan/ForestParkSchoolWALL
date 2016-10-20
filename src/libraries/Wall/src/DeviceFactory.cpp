c2ee9e67ce34dd6cf124cc88caf46091bd0896fd
Matt Plavcan <mwplavcan@gmail.com>
2016-10-20 13:57:31 -0700

    Fixed factory return type for analog expander

--- src/libraries/Wall/src/DeviceFactory.cpp
+++ src/libraries/Wall/src/DeviceFactory.cpp
@@ -1,6 +1,9 @@
 // Wall hardware device generator object
 // Forest Park School Wall 
 //
+#include <SX1509_IO_Expander/src/SparkFunSX1509.h>
+#include <Adafruit_PWM_Servo_Driver_Library/Adafruit_PWMServoDriver.h>
+#include <Adafruit_ADS1X15/Adafruit_ADS1015.h>
 #include "DeviceFactory.h"
 
 SX1509* DeviceFactory::createSX1509Instance(void)
@@ -8,12 +11,12 @@ SX1509* DeviceFactory::createSX1509Instance(void)
     return new SX1509;
 }
 
-Adafruit_ADS1015* DeviceFactory::createADS1015Instance(uint8_t addr)
+Adafruit_ADS1015* DeviceFactory::createADS1015Instance(unsigned char addr)
 {
-    return new Adafruit_PWMServoDriver(addr);
+    return new Adafruit_ADS1015(addr);
 }
 
-Adafruit_PWMServoDriver* DeviceFactory::createPWMinstance(uint8_t addr)
+Adafruit_PWMServoDriver* DeviceFactory::createPWMinstance(unsigned char addr)
 {
     return new Adafruit_PWMServoDriver(addr);
 }
